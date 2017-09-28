aad3d7ef94e653d3e15fb9f14541bdbf8da85b08
tushar bansal <tuskibansal@gmail.com>
2017-09-28 18:33:23 +0530

    api

--- src/config/db.php
+++ src/config/db.php
@@ -3,12 +3,12 @@
 
  	 private $dbhost = 'localhost';
  	 private $dbuser = 'root';
- 	 private $dbpass = '123456';
+ 	 private $dbpass = '12345';
  	 private $dbname = 'slimapp';
 
  	 public function connect(){
- 	 	$mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;"
- 	 	$dbConnection = new PDO(nysql_connect_str, $this->dbuser , $this->dbpass);
+ 	 	$mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname;";
+ 	 	$dbConnection = new PDO($mysql_connect_str, $this->dbuser , $this->dbpass);
  	 	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  	 	return $dbConnection;
  	 }
--- src/routes/customers.php
+++ src/routes/customers.php
@@ -4,7 +4,73 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 
 $app = new \Slim\App;
-
+// select everything
 $app->get('/app/customers',function(Request $request , Response $response){
-	echo "Customers";
+$sql = "SELECT * FROM customers";
+try{
+	$db = new db();
+	$db = $db->connect();
+	$stmt = $db->query($sql);
+	$customers = $stmt->fetchALL(PDO::FETCH_OBJ);
+	$db = null;
+	echo json_encode($customers);
+
+}catch(PDOException $e){
+	echo '{"error": {"text": '.$e->getMessage().'}}';
+}
+
 });
+
+// select by id
+$app->get('/app/customer/{id}',function(Request $request , Response $response){
+	$id = $request->getAttribute('id');
+	$sql = "SELECT * FROM customers WHERE id=$id";
+try{
+	$db = new db();
+	$db = $db->connect();
+	$stmt = $db->query($sql);
+	$customer = $stmt->fetchALL(PDO::FETCH_OBJ);
+	$db = null;
+	echo json_encode($customer);
+
+}catch(PDOException $e){
+	echo '{"error": {"text": '.$e->getMessage().'}}';
+}
+
+});
+
+//add customers
+$app->post('/app/customer/add',function(Request $request , Response $response){
+	$first_name = $request->getParam('first_name');
+	$last_name = $request->getParam('last_name');
+	$phone = $request->getParam('phone');
+	$email = $request->getParam('email');
+	$address = $request->getParam('address');
+	$city = $request->getParam('city');
+	$state = $request->getParam('state');
+
+
+
+	$sql = "INSERT INTO customers (first_name,last_name,phone,email,address,city,state) VALUES (:first_name,:last_name,:phone,:email,:address,:city,:state)";
+try{
+	$db = new db();
+	$db = $db->connect();
+	$stmt = $db->prepare($sql);
+		
+	$stmt = $db->prepare($sql);
+	$stmt->blindparam(':first_name', $first_name);
+	$stmt->blindparam(':last_name', $last_name);
+	$stmt->blindparam(':phone', $phone);
+	$stmt->blindparam(':email', $email);
+	$stmt->blindparam(':address', $address);
+	$stmt->blindparam(':city', $city);
+	$stmt->blindparam(':state', $state);
+	$stmt->execute();
+
+	echo '{"notice": {"text": "Customer Added"}}';
+}catch(PDOException $e){
+	echo '{"error": {"text": '.$e->getMessage().'}}';
+}
+
+});
+
